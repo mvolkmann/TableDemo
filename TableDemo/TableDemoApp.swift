f157b0646d688f7b09dc5fd06ba6650b88122bf9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-08 18:28:43 -0500

    Initial Commit

---
+++ TableDemo/TableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableDemoApp.swift
+//  TableDemo
+//
+//  Created by Mark Volkmann on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
